0ecaee2f59a971106ad5d7a19ff5b757e7acfa20
hiroshi-git147 <116545212+hiroshi-git147@users.noreply.github.com>
2023-02-02 17:14:15 +0900

    Renewal Ramen model

--- RamenSite/src/model/Ramen.java
+++ RamenSite/src/model/Ramen.java
@@ -3,23 +3,8 @@ package model;
 import java.io.Serializable;
 
 public class Ramen implements Serializable {
-	private String name, email, impression, favorite, toppings, gender;
+	private String name, email, impression, favorite, toppings[], gender;
 	private int age, zip1, zip2;
-
-	public Ramen() {}
-	public Ramen(
-			String name, String email, String impression, String favorite, String toppings, String gender,
-			int age, int zip1, int zip2) {
-		this.name = name;
-		this.email = email;
-		this.impression = impression;
-		this.favorite = favorite;
-		this.toppings = toppings;
-		this.gender = gender;
-		this.age = age;
-		this.zip1 = zip1;
-		this.zip2 = zip2;
-	}
 
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
@@ -33,8 +18,8 @@ public class Ramen implements Serializable {
 	public String getFavorite() { return favorite; }
 	public void setFavorite(String favorite) { this.favorite = favorite; }
 
-	public String getToppings() { return toppings; }
-	public void setToppings(String toppings) { this.toppings = toppings; }
+	public String[] getToppings() { return toppings; }
+	public void setToppings(String[] toppings) { this.toppings = toppings; }
 
 	public String getGender() { return gender; }
 	public void setGender(String gender) { this.gender = gender; }
@@ -47,7 +32,5 @@ public class Ramen implements Serializable {
 
 	public int getZip2() { return zip2; }
 	public void setZip2(int zip2) { this.zip2 = zip2; }
-
-
-
+	
 }
